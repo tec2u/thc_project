3fbc8fb6547c20255bb121907949b3d9c18799a4
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2024-11-14 12:16:21 -0300

    debug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -356,7 +356,7 @@
                     <div class="card-total-values mb-3 w-30 mr-auto bg-white p-4 radius-15 d-flex justify-content-between align-items-center">
                         <div>
                             <h4 style='font-weight: 600;'>WITHDRAWAL</h2>
-                            <div>$ {{ number_format($availableComission, 2, '.', ',') }}</div>
+                            <div>$ {{ number_format(abs($withDrawn), 2, '.', ',') }}</div>
                         </div>
                         <div>
                             <h2></h2>
