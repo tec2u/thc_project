0cca3b2b13def43da74e54a3b6f35479dbf0d571
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2024-11-19 12:18:27 -0300

    att

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Traits\CustomLogTrait;
 use App\Models\Wallet;
 use App\Models\Documents;
+use App\Models\PaymentLog;
 use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -466,6 +467,95 @@ class PackageController extends Controller
         } else {
             return false;
         }
+    }
+
+    public function notify(Request $request)
+    {
+        $requestFormated = $request->all();
+
+        // crypto
+        if (isset($requestFormated["id"]) && !isset($requestFormated["node"])) {
+
+            $payment = OrderPackage::where('transaction_wallet', $requestFormated["id"])
+                ->orWhere('transaction_wallet', $requestFormated["merchant_id"])
+                ->first();
+
+            if (!isset($payment)) {
+                return false;
+            }
+
+            if (strtolower($requestFormated["status"]) == 'paid') {
+                $payment->payment_status = 1;
+                $payment->status = 1;
+            }
+
+            if (strtolower($requestFormated["status"]) == 'cancelled' || strtolower($requestFormated["status"]) == 'expired') {
+                $payment->payment_status = 2;
+                $payment->status = 0;
+            }
+
+            $payment->save();
+
+            $log = new PaymentLog;
+            $log->content = $requestFormated["status"];
+            $log->order_package_id = $payment->id;
+            $log->operation = "payment package";
+            $log->controller = "packageController";
+            $log->http_code = "200";
+            $log->route = "/packages/packagepay/notify";
+            $log->status = "success";
+            $log->json = json_encode($request->all());
+            $log->save();
+
+        } else if (isset($requestFormated["node"])) {
+            $payment = OrderPackage::where('transaction_wallet', $requestFormated["id"])
+                ->orWhere('transaction_wallet', $requestFormated["merchant_id"])
+                ->Where('id', $requestFormated["id_order"])
+                ->first();
+
+            if (!isset($payment) || $payment->id != $requestFormated["id_order"]) {
+                return false;
+            }
+
+            if (
+                strtolower($requestFormated["status"]) == 'paid'
+                || strtolower($requestFormated["status"]) == 'overpaid'
+                || strtolower($requestFormated["status"]) == 'underpaid'
+            ) {
+                // price_crypto_payed
+                $payment->payment = $requestFormated["status"];
+                if (isset($requestFormated["price_crypto_payed"])) {
+                    $payment->price_crypto_paid = $requestFormated["price_crypto_payed"];
+                }
+                $payment->payment_status = 1;
+                $payment->status = 1;
+            }
+
+            if (strtolower($requestFormated["status"]) == 'cancelled' || strtolower($requestFormated["status"]) == 'expired') {
+                $payment->payment = $requestFormated["status"];
+                $payment->payment_status = 2;
+                $payment->status = 0;
+            }
+
+            $payment->save();
+
+            if ($payment->package_id == 20 && strtolower($requestFormated["status"]) == 'paid' || strtolower($requestFormated["status"]) == 'overpaid') {
+                // $this->sendPostPayOrder($payment->id);
+            }
+
+            $log = new PaymentLog;
+            $log->content = $requestFormated["status"];
+            $log->order_package_id = $payment->id;
+            $log->operation = "payment package";
+            $log->controller = "packageController";
+            $log->http_code = "200";
+            $log->route = "/packages/packagepay/notify";
+            $log->status = "success";
+            $log->json = json_encode($request->all());
+            $log->save();
+        }
+
+        return response("OK", 200);
     }
 
     public function sendPostPayOrder($id_order)
