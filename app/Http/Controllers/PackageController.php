42993eef0f59a6e5ffb983607796c2f6e296e999
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2024-11-20 15:56:55 -0300

    att

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -719,6 +719,8 @@ class PackageController extends Controller
         // dd($order);
         $postNode = $this->genUrlCryptoNode($request->method, $order);
 
+        return response()->json($postNode);
+        exit;
         if (!$postNode) {
             // dd($postNode);
             $order = OrderPackage::where('id', $request->id)->first();
